e3774194b0b958b56f78cd9c3af4f883e9f10344
diemnguyen244 <nguyentranhoaidiem@gmail.com>
2023-01-03 22:23:52 +0700

    Create: Gallery Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,9 @@
-import { Blog } from "./components/Blog";
-import Experiences from "./components/Experiences";
-import Footer from "./components/Footer";
-import Hero from "./components/Hero";
-import Navbar from "./components/Navbar";
-import Offer from "./components/Offer";
-import Popular from "./components/Popular";
-import Trip from "./components/Trip";
+import Gallery from "./components/Gallery";
 
 const App = () => {
   return (
     <div>
-      <Navbar />
-      <Hero />
-      <Popular />
-      <Offer />
-      <Blog />
-      <Trip />
-      <Experiences />
-      <Footer />
+      <Gallery />
     </div>
   );
 };
